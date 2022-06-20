4c2cb030fbe4041285dd8a0254d650809ecb6b72
Eduardo Santos <eduardo@eduardosan.com>
2022-06-20 17:27:53 -0300

    Project skeleton files with build scripts

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM python:3.9 as base
+
+WORKDIR /usr/src/app
+ENV PYTHONIOENCODING  utf-8
+ARG TIMEZONE=America/Sao_Paulo
+ARG LOCALE=pt_BR.UTF-8
+
+# Set Timezone
+RUN echo "Setting default timezone to $TIMEZONE"
+ENV TZ=$TIMEZONE
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+RUN echo "$TIMEZONE" > /etc/timezone; dpkg-reconfigure -f noninteractive tzdata
+
+# Configure locale
+RUN echo "Set default locale to $LOCALE"
+RUN apt-get update && apt-get install -y locales && rm -rf /var/lib/apt/lists/*
+RUN echo "$LOCALE UTF-8" >> /etc/locale.gen
+RUN locale-gen
+RUN export LANGUAGE="$LOCALE"; export LANG="$LOCALE"; export LC_ALL="$LOCALE"; locale-gen "$LOCALE"; DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD [ "python", "./setup.py" , "develop" ]
+
+FROM base as testing
+# Need to clean these files in order for the tests to work
+RUN find . -name '*.py[odc]' -type f -delete
+RUN find . -name '__pycache__' -type d -delete
+RUN rm -rf *.egg-info .cache .eggs build dist dists
+
+# Run testing dependencies for the container
+RUN pip install --no-cache-dir -e ".[testing]"
+
---
+++ setup.py
@@ -0,0 +1,49 @@
+import os.path
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+with open(os.path.join(here, 'VERSION')) as f:
+    VERSION = f.read()
+
+requires = [
+    'requests'
+]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-flake8',
+    'pytest-cov',
+    'mock',
+    'gitchangelog',
+]
+
+setup(
+    name='cd4ml',
+    version=VERSION,
+    description='Library to enable CD4ML steps',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 1 - Planning'
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Build Tools'
+    ],
+    keywords="machine learning mlops",
+    packages=find_packages(),
+    include_package_data=True,
+    url='',
+    license='BSD',
+    author='Eduardo Santos',
+    author_email='eduardo@eduardosan.com',
+    extras_require={
+        'testing': tests_require
+    },
+    install_requires=requires,
+)
